5c31315850526b4162cc4d17ebe117c6ecb5de6e
Lu27600 <lucienmonod@yahoo.fr>
2015-12-02 18:38:34 +0100

    WSC

--- Symfony/src/Maxcraft/DefaultBundle/Controller/DefaultController.php
+++ Symfony/src/Maxcraft/DefaultBundle/Controller/DefaultController.php
@@ -2,14 +2,20 @@
 
 namespace Maxcraft\DefaultBundle\Controller;
 
+use NathemWS\PingRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $this->get('WSC')->sendRequest(new PingRequest());
+
         return $this->render('MaxcraftDefaultBundle:Default:index.html.twig');
     }
 
 
+
 }
+
+
---
+++ Symfony/src/Maxcraft/DefaultBundle/Websocket/BuilderRequest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 02/12/15
+ * Time: 18:29
+ */
+
+namespace Maxcraft\DefaultBundle\Websocket;
+
+
+use NathemWS\NathemWSRequest;
+
+class BuilderRequest extends NathemWSRequest
+{
+
+    public function getType()
+    {
+        // TODO: Implement getType() method.
+    }
+
+    public function buildData()
+    {
+        // TODO: Implement buildData() method.
+    }
+
+    public function onResponse($data)
+    {
+        // TODO: Implement onResponse() method.
+    }
+}
