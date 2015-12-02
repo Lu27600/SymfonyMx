75556fc2ee40ccdb09d9fd81b680a351e54ee2f6
Lu27600 <lucienmonod@yahoo.fr>
2015-12-02 18:55:26 +0100

    WSC

--- Symfony/src/Maxcraft/DefaultBundle/Controller/DefaultController.php
+++ Symfony/src/Maxcraft/DefaultBundle/Controller/DefaultController.php
@@ -4,12 +4,19 @@ namespace Maxcraft\DefaultBundle\Controller;
 
 use NathemWS\PingRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use WebSocket\ConnectionException;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $this->get('WSC')->sendRequest(new PingRequest(), 'toto');
+
+        try {
+            $this->get('WSC')->sendRequest(new PingRequest(), 'toto');
+        } catch(ConnectionException $e){
+            return new Response($e->getMessage());
+        }
 
         return $this->render('MaxcraftDefaultBundle:Default:index.html.twig');
     }
