1e3d92aaae37b659e40b3e220f6cbf305d7e4580
Lu27600 <lucienmonod@yahoo.fr>
2016-03-16 19:15:06 +0100

    PHP adminMenuAction()

--- Symfony/src/Maxcraft/DefaultBundle/Controller/FactionController.php
+++ Symfony/src/Maxcraft/DefaultBundle/Controller/FactionController.php
@@ -11,7 +11,41 @@ namespace Maxcraft\DefaultBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class FactionController extends Controller
-{
+class FactionController extends Controller {
 
+    public function infosAction($factionTag){
+
+        if ($faction = $this->getDoctrine()->getRepository('MaxcraftDefaultBundle:Faction')->findOneByTag($factionTag)){
+
+            $message = 'La Faction '. strtoupper($factionTag).' n\'a pas été trouvée';
+
+            return $this->render('MaxcraftDefaultBundle:Others:error.html.twig', array(
+                'content' => $message
+            ));
+        }
+
+        if(!($this->get('security.context')->isGranted('ROLE_USER'))) {
+
+            $visitor = true;
+            $ismine = false;
+        }
+        else {
+            $visitor = false;
+            if($faction == $this->getUser()->getFaction())
+            {
+                $ismine = true;
+
+            }
+            else
+            {
+                $ismine = false;
+            }
+        }
+
+        $rep = $this->getDoctrine()->getRepository('MaxcraftDefaultBundle:User');
+        $members= $rep->findByFaction($faction);
+
+        //WARS
+        //TODO wars
+    }
 }
